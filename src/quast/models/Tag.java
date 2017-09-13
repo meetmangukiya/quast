2bc38d0bddfb60d681410fb9cd5f13193d8447bc
Meet Mangukiya <meetmangukiya98@gmail.com>
2017-09-13 19:35:21 +0530

    Tag.java: Load description from database

--- src/quast/models/Tag.java
+++ src/quast/models/Tag.java
@@ -1,8 +1,24 @@
 package quast.models;
 
+import java.sql.SQLException;
+
+import quast.db.Helper;
+
 public class Tag {
     private String name;
     private String description;
 
-    
+    public Tag(String name) {
+        name = name;
+        Helper db = new Helper();
+        try {
+            description = db.retrieve(String.format(
+                "SELECT description FROM tags WHERE name=%s",
+                name)).getString(1);
+        }
+        catch (SQLException ex) {
+            System.out.println("Exception: " + ex);
+            ex.printStackTrace();
+        }
+    }
 }

f43f540bbe32ed564d689cc7f86c231b7e79d176
Meet Mangukiya <meetmangukiya98@gmail.com>
2017-09-13 19:40:23 +0530

    Question.java: Load question data from database

--- src/quast/models/Question.java
+++ src/quast/models/Question.java
@@ -1,7 +1,10 @@
 package quast.models;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 
+import quast.db.Helper;
 import quast.models.User;
 import quast.models.Answer;
 
@@ -9,7 +12,6 @@ import quast.models.Answer;
  * Question class representing a question.
  */
 public class Question {
-
     public User author;
     public String title;
     public String body;
@@ -21,7 +23,25 @@ public class Question {
      * Construct question from qid.
      * @param qid Question ID
      */
-    public Question(int qid) {}
+    public Question(int qid) {
+        qid = qid;
+        Helper db = new Helper();
+        try {
+            ResultSet rs = db.retrieve(String.format(
+                "SELECT author, title, body, upvotes, downvotes " +
+                "FROM questions WHERE question=%d",
+                qid));
+            author = rs.getString(1);
+            title = rs.getString(2);
+            body = rs.getString(3);
+            upvotes = rs.getInt(4);
+            downvotes = rs.getInt(5);
+        }
+        catch (SQLException ex) {
+            System.out.println("Exception: " + ex);
+            ex.printStackTrace();
+        }
+    }
 
     public void upvote() {}
 

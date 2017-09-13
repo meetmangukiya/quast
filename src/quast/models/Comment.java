d8eee370a3e4fde4868cacaa13b92b1045a41afd
Meet Mangukiya <meetmangukiya98@gmail.com>
2017-09-13 19:51:38 +0530

    Comment.java: Make all fields protected
    To make it inheritable in QuestionComment and AnswerComment classes.

--- src/quast/models/Comment.java
+++ src/quast/models/Comment.java
@@ -6,16 +6,16 @@ import quast.models.User;
  * Comment class representing a comment.
  */
 public class Comment {
-    private User author;
-    private int upvote;
-    private int downvotes;
-    private String body;
-    
+    protected User author;
+    protected int upvote;
+    protected int downvotes;
+    protected String body;
+
     /**
      * upvote the comment and update the data in database
      */
     public void upvote() {};
-    
+
     /**
      * downvote the comment and update the data in database
      */

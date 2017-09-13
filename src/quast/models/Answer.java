8ab5eea2f6cfdf1017be90022be11fa30030c1ea
Meet Mangukiya <meetmangukiya98@gmail.com>
2017-09-13 19:47:22 +0530

    Answer.java: Load answer data from database

--- src/quast/models/Answer.java
+++ src/quast/models/Answer.java
@@ -1,5 +1,8 @@
 package quast.models;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 import quast.models.User;
 
 /**
@@ -17,7 +20,27 @@ public class Answer{
      * @param qid Question ID
      * @param aid Answer ID
      */
-    public Answer(int qid, int aid) {}
+    public Answer(int qid, int aid) {
+        qid = qid;
+        aid = aid;
+        Helper db = new Helper();
+        try {
+            ResultSet rs = db.retrieve(String.format(
+                "SELECT author, body, upvotes, downvotes FROM answers" +
+                "WHERE qid=%d AND aid=%d"
+                ),
+                qid, aid
+            );
+            author = rs.getString(1);
+            body = rs.getString(2);
+            upvotes = rs.getInt(3);
+            downvotes = rs.getInt(4);
+        }
+        catch (SQLException ex) {
+            System.out.println("Exception: " + ex);
+            ex.printStackTrace();
+        }
+    }
 
     public void upvote() {}
 

c10244554ef6f7209828aebecb14f42f79206cbc
Meet Mangukiya <meetmangukiya98@gmail.com>
2017-09-23 18:22:41 +0530

    Add answers method to Question, and use pool in Question and Answers

--- quast/models/Answer.py
+++ quast/models/Answer.py
@@ -1,4 +1,5 @@
 from psycopg2.extensions import connection
+from psycopg2.pool import ThreadedConnectionPool
 
 from quast.models.User import User
 
@@ -12,22 +13,26 @@ class Answer:
                  body: str,
                  upvotes: int,
                  downvotes: int,
-                 qid: int):
+                 qid: int,
+                 pool: ThreadedConnectionPool=None):
         self._author = author
         self._body = body
         self._upvotes = upvotes
         self._downvotes = downvotes
         self._qid = qid
+        self._pool = pool
 
     @staticmethod
     def from_qid_author(qid: int,
                         author: User,
-                        conn: connection):
+                        pool: ThreadedConnectionPool=None):
+        conn = pool.getconn()
         with conn.cursor() as curs:
             curs.execute("SELECT body, upvotes, downvotes, "
-                         "FROM answers"
+                         "FROM answers "
                          "WHERE author=%(username)s AND qid=%(qid)s",
                          {'username': User.username, 'qid': qid})
             body, upvotes, downvotes = curs.fetchone()
+        pool.putconn(conn)
         return Answer(author=author, body=body, upvotes=upvotes,
-                      downvotes=downvotes, qid=qid)
+                      downvotes=downvotes, qid=qid, pool=pool)
--- quast/models/Question.py
+++ quast/models/Question.py
@@ -1,5 +1,7 @@
 from psycopg2.extensions import connection
+from psycopg2.pool import ThreadedConnectionPool
 
+from quast.models.Answer import Answer
 from quast.models.User import User
 
 class Question:
@@ -12,20 +14,40 @@ class Question:
                  title: str=None,
                  body: str=None,
                  upvotes: int=0,
-                 downvotes: int=0):
+                 downvotes: int=0,
+                 pool: ThreadedConnectionPool=None):
         self._author = author
         self._title = title
         self._body = body
         self._upvotes = upvotes if upvotes else 0
         self._downvotes = downvotes if downvotes else 0
+        self._pool = pool
 
     @staticmethod
     def from_qid(qid: int,
-                 conn: connection):
+                 pool: ThreadedConnectionPool):
+        conn = pool.getconn()
         with conn.cursor() as curs:
             curs.execute("SELECT author, title, description, upvotes, downvotes "
                          "FROM questions "
                          "WHERE qid=%s", (qid, ))
             author, title, body, upvotes, downvotes = curs.fetchone()
+        pool.putconn(conn)
         return Question(author=author, title=title, body=body, upvotes=upvotes,
-                        downvotes=downvotes)
+                        downvotes=downvotes, pool=pool)
+
+    def answers(self):
+        """
+        Return a list of ``quast.models.Answer`` object.
+        """
+        conn = self._pool.getconn()
+        answers = []
+        with conn.cursor() as curs:
+            curs.execute("SELECT author, body, upvotes, downvotes FROM answers "
+                         "WHERE qid=%s", (qid, ))
+            for author, body, upvotes, downvotes in curs.fetchone():
+                answers.append(Answer(author=author, body=body, upvotes=upvotes
+                                      downvotes=downvotes, qid=qid,
+                                      pool=self._pool))
+        self._pool.putconn(conn)
+        return answers

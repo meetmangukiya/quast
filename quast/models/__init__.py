201d223c6b943b614b2d024f2364fdf454e7d512
Meet Mangukiya <meetmangukiya98@gmail.com>
2017-09-23 21:14:44 +0530

    quast.models: Do not import all the classes in the init file

--- quast/models/__init__.py
+++
@@ -1,5 +0,0 @@
-from quast.models.User import User
-from quast.models.Answer import Answer
-from quast.models.Question import Question
-from quast.models.Tag import Tag
-from quast.models.Comments import QuestionComment, AnswerComment

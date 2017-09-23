702006d70d79e8580c1a5e0e247e31b35d5557c8
Meet Mangukiya <meetmangukiya98@gmail.com>
2017-09-23 19:28:40 +0530

    destruct.sql: Instructions to destroy complete database

---
+++ destruct.sql
@@ -0,0 +1,15 @@
+-- DESTROY COMPLETE DATABASE
+-- =========================
+
+DROP TABLE answer_comments;
+DROP TABLE question_comments;
+DROP TABLE question_tags;
+DROP TABLE tags;
+DROP TABLE answer_downvotes;
+DROP TABLE answer_upvotes;
+DROP TABLE answers;
+DROP TABLE question_upvotes;
+DROP TABLE question_downvotes;
+DROP TABLE questions;
+DROP TABLE followers;
+DROP TABLE users;

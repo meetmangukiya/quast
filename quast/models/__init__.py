ea1a72852f9ce6bb0f4c3f0cc10f55f249b158c8
Meet Mangukiya <meetmangukiya98@gmail.com>
2017-09-23 17:38:19 +0530

    quast.models: Import all the classes in init

---
+++ quast/models/__init__.py
@@ -0,0 +1,5 @@
+from quast.models.User import User
+from quast.models.Answer import Answer
+from quast.models.Question import Question
+from quast.models.Tag import Tag
+from quast.models.Comments import QuestionComment, AnswerComment

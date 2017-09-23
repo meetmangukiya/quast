22df62936ebfcd7718deff41a017149c2985617f
Meet Mangukiya <meetmangukiya98@gmail.com>
2017-09-23 18:08:15 +0530

    User: Add get_followers function and add pool attrib
    Initialise each User object with pool attribute and use that pool to get
    the connections that are needed by the object.

--- quast/models/User.py
+++ quast/models/User.py
@@ -1,4 +1,5 @@
 from psycopg2.extensions import connection
+from psycopg2.pool import ThreadedConnectionPool
 
 class User:
     """
@@ -8,17 +9,33 @@ class User:
     def __init__(self,
                  username: str=None,
                  credits: int=0,
-                 bio: str=None):
+                 bio: str=None,
+                 pool: ThreadedConnectionPool=None):
         self._username = username
         self._credits = int(credits)
         self._bio = str(bio) if bio else ''
+        self._pool = pool
 
     @staticmethod
     def from_username(username: str,
-                      conn: connection):
+                      pool: connection):
+        conn = pool.getconn()
         with conn.cursor() as curs:
-            curs.execute("""SELECT username, credits, bio
-                         FROM users WHERE username=%s""",
+            curs.execute("SELECT username, credits, bio "
+                         "FROM users WHERE username=%s",
                          (str(username), ))
             username, credits, bio = curs.fetchone()
-        return User(username=username, credits=credits, bio=bio)
+        pool.putconn(conn)
+        return User(username=username, credits=credits, bio=bio, pool=pool)
+
+    def get_followers(self):
+        """
+        Return a list of users that follow the given user.
+        """
+        conn = self._pool.getconn()
+        with conn.cursor() as curs:
+            curs.execute("SELECT followed_by FROM users "
+                         "WHERE following_to=%s", (self._username, ))
+            followers = list(map(lambda x: x[0], curs.fetchall()))
+        self._pool.putconn(conn)
+        return followers

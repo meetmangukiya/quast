403f8c857031f69ed4712d9e855be009ffb30840
Meet Mangukiya <meetmangukiya98@gmail.com>
2017-09-23 17:38:57 +0530

    setup.py: Add setup.py file

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+if __name__ == '__main__':
+    setup(
+        name='quast',
+        version='0.0.1',
+        description='Quast - On the quast to answer your questions and make your life easier',
+        author='The quast developers',
+        author_email='meetmangukiya98@gmail.com',
+        package=find_packages()
+    )

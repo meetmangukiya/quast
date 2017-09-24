0db00d3f34092e85b46e7d37a30cfe92c0eb1c29
Meet Mangukiya <meetmangukiya98@gmail.com>
2017-09-24 08:33:19 +0530

    quast/app.py: Move methods parameter to decorator
    Fixes https://github.com/meetmangukiya/quast/issues/38

--- quast/app.py
+++ quast/app.py
@@ -16,8 +16,8 @@ POOL = ThreadedConnectionPool(
 
 app = Flask(__name__)
 
-@app.route("/question/<int:qid>")
-def question_info(qid, methods=['GET']):
+@app.route("/question/<int:qid>", methods=['GET'])
+def question_info(qid):
     """
     Return all question information.
     """
@@ -27,24 +27,24 @@ def question_info(qid, methods=['GET']):
     data['answers'] = list(map(lambda x: x.as_dict(), answers))
     return jsonify(data)
 
-@app.route("/question/<int:qid>/answer/<answerer>")
-def answer_info(qid, answerer, methods=['GET']):
+@app.route("/question/<int:qid>/answer/<answerer>", methods=['GET'])
+def answer_info(qid, answerer):
     """
     Return all answer information.
     """
     answer = Answer.from_qid_author(qid, answerer, POOL)
     return jsonify(answer.as_dict())
 
-@app.route("/users/<username>/followers")
-def user_followers(username, methods=['GET']):
+@app.route("/users/<username>/followers", methods=['GET'])
+def user_followers(username):
     """
     Return list of followers of user ``username``.
     """
     user = User.from_username(username, POOL)
     return jsonify(user.get_followers())
 
-@app.route("/user/<username>/questions")
-def user_questions(username, methods=['GET']):
+@app.route("/user/<username>/questions", methods=['GET'])
+def user_questions(username):
     """
     Return list of questions asked by user ``username``.
     """

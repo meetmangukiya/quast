59ce307c6f2453fd2e5f7de080bd5651ff75ca3e
Meet Mangukiya <meetmangukiya98@gmail.com>
2017-09-27 20:07:17 +0530

    app: Add /user/me endpoint and construct json using request.get_json everywhere

--- quast/app.py
+++ quast/app.py
@@ -4,7 +4,7 @@ import uuid
 
 from flask import Flask
 from flask import jsonify
-from flask import request, session
+from flask import request, session, make_response
 from flask_session import Session
 from psycopg2.pool import ThreadedConnectionPool
 
@@ -52,7 +52,17 @@ def answer_info(qid, answerer):
     return jsonify(answer.as_dict())
 
 
-@app.route("/users/<username>/followers", methods=['GET'])
+@app.route("/user/me", methods=['GET'])
+def current_user():
+    """
+    Return information of logged in user.
+    """
+    assert session.get('logged_in')
+    user = User.from_username(session['username'], POOL)
+    return jsonify(user.as_dict())
+
+
+@app.route("/user/<username>/followers", methods=['GET'])
 def user_followers(username):
     """
     Return list of followers of user ``username``.
@@ -90,11 +100,12 @@ def login():
     """
     Login the user.
     """
-    if authenticate(request.json.get('username'), request.json.get('password'), POOL):
+    json = request.get_json()
+    if authenticate(json.get('username'), json.get('password'), POOL):
         session["logged_in"] = True
-        session["username"] = request.json.get('username')
+        session["username"] = json.get('username')
         return jsonify({"status": "success"})
-    return jsonify({"status": "failed"})
+    return make_response(jsonify({"status": "failed"}), 401)
 
 
 @app.route("/logout", methods=["POST"])
@@ -111,9 +122,10 @@ def register_new_user():
     """
     Register a new user.
     """
+    json = request.get_json()
+    username = json.get('username')
+    password = json.get('password')
     salt = uuid.uuid4().bytes
-    username = request.json.get('username')
-    password = request.json.get('password')
     assert username is not None or password is not None
 
     pwdhash = hashlib.sha1()
@@ -140,9 +152,10 @@ def create_question():
     Create a new question.
     """
     author = session['username']
-    title = request.json.get('title')
-    body = request.json.get('body')
-    tags = request.json.get('tags')
+    json = request.get_json()
+    title = json.get('title')
+    body = json.get('body')
+    tags = json.get('tags')
     question = Question.create(author=author, title=title, body=body, tags=tags,
                                pool=POOL)
     return jsonify(question.as_dict())
@@ -154,8 +167,9 @@ def create_tag():
     """
     Create a tag.
     """
-    name = request.json.get('name')
-    description = request.json.get('description')
+    json = request.get_json()
+    name = json.get('name')
+    description = json.get('description')
     tag = Tag.create(name=name, description=description, pool=POOL)
     return jsonify(tag.as_dict())
 

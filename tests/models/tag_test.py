d4335809fc25e2c1a3fe2dc10feda6ecb871152f
Meet Mangukiya <meetmangukiya98@gmail.com>
2017-09-28 21:34:16 +0530

    Add tests for Tag model

---
+++ tests/models/tag_test.py
@@ -0,0 +1,35 @@
+import unittest
+
+from psycopg2.pool import ThreadedConnectionPool
+
+from quast.models.Question import Question
+from quast.models.Tag import Tag
+
+from tests.Base import QuastTestCase
+
+class TestTag(QuastTestCase):
+    def test_from_name(self):
+        tag = Tag.from_name('java', self.pool)
+        self.assertEqual(tag._name, 'java')
+        self.assertEqual(tag._description,
+                         'java is one of the most popular programming '
+                         'languages.')
+
+    def test_create(self):
+        Tag.create('test_tag', 'description', self.pool)
+        retrieved_tag = Tag.from_name('test_tag', self.pool)
+        expected_tag = Tag('test_tag', 'description', self.pool)
+        self.assertEqual(retrieved_tag, expected_tag)
+
+    def test_questions(self):
+        tag = Tag.create(name='test_tag', description='..', pool=self.pool)
+        question = Question.create(title='asa', author='meetmangukiya', body='',
+                                   tags=['test_tag'], pool=self.pool)
+        self.assertEqual(len(tag.questions()), 1)
+
+    def test_as_dict(self):
+        tag = Tag.from_name('java', self.pool)
+        self.assertEqual({'name': 'java',
+                          'description': 'java is one of the most popular '
+                                         'programming languages.'},
+                         tag.as_dict())

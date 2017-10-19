02906bb3205ccb7bf809c85530d613c798295368
Meet Mangukiya <meetmangukiya98@gmail.com>
2017-10-19 12:22:59 +0530

    manage_db: Create manage_db

---
+++ manage_db.sh
@@ -0,0 +1,13 @@
+set -e -x
+
+for i in $#
+do
+    case "$1" in
+        ("create") createdb quast;;
+        ("setup") psql -U postgres -d quast -a -f sql/setup.sql -v "ON_ERROR_STOP=1";;
+        ("populate") psql -U postgres -d quast -a -f sql/populate.sql -v "ON_ERROR_STOP=1";;
+        ("destruct") psql -U postgres -d quast -a -f sql/destruct.sql -v "ON_ERROR_STOP=1";;
+        ("drop") dropdb quast;;
+    esac
+    shift;
+done
